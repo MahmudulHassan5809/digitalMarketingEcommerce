e60b51cab037d719d1dd69a683d974c4ac32094b
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-02-23 01:16:16 +0600

    Dashboard template

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (HomeView)
+from .views import HomeView,CategoryProductView
 
 app_name = 'store'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('product/category/<int:id>', CategoryProductView.as_view(), name="category_product")
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.views import View
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'store/index.html')
+
+
+class CategoryProductView(View):
+	def get(self,request,*args,**kwargs):
+		return HttpResponse('okkkkkkkkkkkkkkkkkkk')
